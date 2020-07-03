b0bd292c7e4b8c5c6ffe4616787667a04859e62d
Oksana Tkachenko <coatceatl@gmail.com>
2020-07-03 18:47:10 +0300

    added options from db.json

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import ChartPage from './ChartPage';
+import { ChartPage } from './ChartPage';
 
 function App() {
   const [fetchData, setFetchData] = useState()
   const [series, setSeries] = useState([])
+  const [options, setOptions] = useState({})
   const newSeries = []
 
   useEffect(() => {
@@ -15,14 +16,18 @@ function App() {
         //series
         const newDataSeries = Object.values(result)[1]
         const newData = Object.values(newDataSeries)[0].data
-
         const data = newData.map((s) => {
           return s
         });
         newSeries.push({ data: data });
 
+        //options
+        const newDataOptions = Object.values(result)[0]
+        console.log(newDataOptions)
+
         setFetchData(data)
         setSeries(newSeries)
+        setOptions(newDataOptions)
       } else {
         console.log("Ошибка HTTP: " + response.status);
       }
@@ -33,7 +38,7 @@ function App() {
   return (
     <>
       <h1>React Chart</h1>
-      <ChartPage fetchData={fetchData} series={series} />
+      <ChartPage fetchData={fetchData} series={series} options={options} />
     </>
   );
 }
--- src/ChartPage.js
+++ src/ChartPage.js
@@ -1,44 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import Chart from "react-apexcharts";
 
-class ChartPage extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      options: {
-        chart: {
-          height: 350,
-          type: 'bar',
-        },
-        dataLabels: {
-          enabled: false
-        },
-        title: {
-          text: 'Ajax Example',
-        },
-        noData: {
-          text: 'Loading...'
-        }
-      },
-      series: []
-    };
-  }
-
-  render() {
-    return (
-      <div className="row">
-        <div className="mixed-chart">
-          <Chart
-            options={this.state.options}
-            series={this.props.series}
-            type="bar"
-            width="500"
-          />
-        </div>
+export const ChartPage = ({ options, series }) => {
+  return (
+    <div className="row">
+      <div className="mixed-chart">
+        <Chart
+          options={options}
+          series={series}
+          type="bar"
+          width="500"
+        />
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default ChartPage;
