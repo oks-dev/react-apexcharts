2d5e82980917e6ac27d660d81a0ab8c9f375ba0a
Oksana Tkachenko <coatceatl@gmail.com>
2020-07-03 18:39:23 +0300

    added db.json and fetch data from mock backend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ChartPage from './ChartPage';
 
 function App() {
+  const [fetchData, setFetchData] = useState()
+  const [series, setSeries] = useState([])
+  const newSeries = []
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('http://localhost:3000/db.json');
+      if (response.ok) {
+        let result = await response.json();
+
+        //series
+        const newDataSeries = Object.values(result)[1]
+        const newData = Object.values(newDataSeries)[0].data
+
+        const data = newData.map((s) => {
+          return s
+        });
+        newSeries.push({ data: data });
+
+        setFetchData(data)
+        setSeries(newSeries)
+      } else {
+        console.log("Ошибка HTTP: " + response.status);
+      }
+    }
+    fetchData();
+  }, [])
+
   return (
     <>
       <h1>React Chart</h1>
-      <ChartPage />
+      <ChartPage fetchData={fetchData} series={series} />
     </>
   );
 }
--- src/ChartPage.js
+++ src/ChartPage.js
@@ -8,33 +8,33 @@ class ChartPage extends Component {
     this.state = {
       options: {
         chart: {
-          id: "basic-bar"
+          height: 350,
+          type: 'bar',
         },
-        xaxis: {
-          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+        dataLabels: {
+          enabled: false
+        },
+        title: {
+          text: 'Ajax Example',
+        },
+        noData: {
+          text: 'Loading...'
         }
       },
-      series: [
-        {
-          name: "series-1",
-          data: [30, 40, 45, 50, 49, 60, 70, 91]
-        }
-      ]
+      series: []
     };
   }
 
   render() {
     return (
-      <div className="app">
-        <div className="row">
-          <div className="mixed-chart">
-            <Chart
-              options={this.state.options}
-              series={this.state.series}
-              type="bar"
-              width="500"
-            />
-          </div>
+      <div className="row">
+        <div className="mixed-chart">
+          <Chart
+            options={this.state.options}
+            series={this.props.series}
+            type="bar"
+            width="500"
+          />
         </div>
       </div>
     );
