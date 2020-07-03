07617ec39ecb82cc5f17df2b8d6a32226f2f4b51
Oksana Tkachenko <coatceatl@gmail.com>
2020-07-03 20:53:12 +0300

    added data in App component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import { ChartPage } from './ChartPage';
 
-function App() {
-  const [fetchData, setFetchData] = useState()
+const url = 'http://localhost:3000/db.json';
+
+const App = () => {
   const [series, setSeries] = useState([])
   const [options, setOptions] = useState({})
-  const newSeries = []
 
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch('http://localhost:3000/db.json');
+      const response = await fetch(url);
+      const newSeries = []
       if (response.ok) {
         let result = await response.json();
 
         //series
         const newDataSeries = Object.values(result)[1]
         const newData = Object.values(newDataSeries)[0].data
-        const data = newData.map((s) => {
-          return s
-        });
+        const data = newData.map(s => s);
         newSeries.push({ data: data });
 
         //options
         const newDataOptions = Object.values(result)[0]
-        console.log(newDataOptions)
 
-        setFetchData(data)
         setSeries(newSeries)
         setOptions(newDataOptions)
       } else {
@@ -38,7 +35,7 @@ function App() {
   return (
     <>
       <h1>React Chart</h1>
-      <ChartPage fetchData={fetchData} series={series} options={options} />
+      <ChartPage series={series} options={options} />
     </>
   );
 }
