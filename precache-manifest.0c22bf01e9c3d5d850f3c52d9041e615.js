67bfc0d5dba67249fa3f669a4facc404d41d4c9a
Oksana Tkachenko <coatceatl@gmail.com>
2020-08-16 14:19:20 +0300

    Updates

---
+++ precache-manifest.0c22bf01e9c3d5d850f3c52d9041e615.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74281f4bf871fb45bf3f6912b76577c1",
+    "url": "/react-apexcharts/index.html"
+  },
+  {
+    "revision": "2808707137c0bce9517d",
+    "url": "/react-apexcharts/static/css/main.991028b6.chunk.css"
+  },
+  {
+    "revision": "1eac804b329cb5722ed4",
+    "url": "/react-apexcharts/static/js/2.e79debf0.chunk.js"
+  },
+  {
+    "revision": "acff744c5c5e21e2b14a11e29451820a",
+    "url": "/react-apexcharts/static/js/2.e79debf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2808707137c0bce9517d",
+    "url": "/react-apexcharts/static/js/main.5dc4d571.chunk.js"
+  },
+  {
+    "revision": "578f5dcf091f2a1661df",
+    "url": "/react-apexcharts/static/js/runtime-main.c57255bd.js"
+  }
+]);
